2349a19f9677508b02f96fbc95c51a9c6af2a468
E. Nasir <ourmathnet@gmail.com>
2026-02-10 10:53:54 +0100

    add models of Book, Loan, and Member

---
+++ app/Models/Loan.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Loan extends Model
+{
+
+    protected $fillable = [
+        'book_id',
+        'member_id',
+        'loaned_at',
+        'due_at',
+        'returned_at',
+    ];
+
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
+
+    public function member()
+    {
+        return $this->belongsTo(Member::class);
+    }
+}
---
+++ app/Models/Member.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Member extends Model
+{
+    protected $fillable = [
+        'name',
+        'email',
+        'membership_date',
+    ];
+}
