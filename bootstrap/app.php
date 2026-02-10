e81d6ce04ec6af59f129fd421ac4d65ca3fb8880
E. Nasir <ourmathnet@gmail.com>
2026-02-10 10:57:36 +0100

    define the api route to start working

---
+++ bootstrap/app.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',//without this i have got 404
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',//todo => use it in the container checjer
+    )
+    ->withMiddleware(function (Middleware $middleware): void {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
+    })->create();
