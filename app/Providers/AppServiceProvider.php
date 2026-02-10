1bb22d0480c9178ec9d1c21aab782f6a243faf06
E. Nasir <ourmathnet@gmail.com>
2026-02-10 14:10:58 +0100

    bind LoanRepositoryInterface.php to EloquentLoanRepository.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use App\Repositories\BookRepositoryInterface;
 use App\Repositories\EloquentBookRepository;
+use App\Repositories\EloquentLoanRepository;
+use App\Repositories\LoanRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(BookRepositoryInterface::class, EloquentBookRepository::class);
+        $this->app->bind(LoanRepositoryInterface::class, EloquentLoanRepository::class);
     }
 
     /**
