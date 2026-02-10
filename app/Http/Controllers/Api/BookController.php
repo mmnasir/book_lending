0d6bd22303cee78c11218c9ea0f9cab85e8e207b
E. Nasir <ourmathnet@gmail.com>
2026-02-10 11:52:40 +0100

    add the needed controllers

---
+++ app/Http/Controllers/Api/BookController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Repositories\BookRepositoryInterface;
+use Illuminate\Http\JsonResponse;
+
+class BookController  extends Controller
+{
+    //todo construct
+    public function __construct(private readonly BookRepositoryInterface $books)
+    {
+    }
+    public function index(): JsonResponse
+    {
+        return response()->json([
+            'data' => $this->books->all(),//todo need a seeder or post method to add books
+        ]);
+    }
+
+}
---
+++ app/Http/Controllers/Api/LoanController.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateLoanRequest;
+use App\Models\Loan;
+use Illuminate\Http\JsonResponse;
+use App\Services\LoanService;
+use App\Exceptions\OutOfStockException;
+
+
+class LoanController extends Controller
+{
+    //todo construct
+    public function __construct(private readonly LoanService $loanService)
+    {
+    }
+
+    public function store(CreateLoanRequest $request): JsonResponse
+    {
+        //todo validate request
+        //todo create loan
+        $loan = $this->loanService->createLoan(
+            (int) $request->validated('book_id'),
+            (int) $request->validated('member_id'),
+        );
+        return response()->json(['data' => $loan], 201);
+    }
+
+    public function return(int $id): JsonResponse
+    {
+        //todo validate request
+        //todo return loan
+        $this->loanService->returnLoan($id);
+
+        return response()->json(['message' => 'Returned!!Done!!']);
+    }
+
+}
+
